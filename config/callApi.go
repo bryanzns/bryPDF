e7796ab95416ba63401885784109003cc16f4184
Peckerzns <bryan2011adams@gmail.com>
2025-06-21 20:17:50 -0300

    callApi.go

--- config/callApi.go
+++ config/callApi.go
@@ -8,18 +8,18 @@ import (
 
 	"google.golang.org/genai"
 )
-func CallApi(mensageiro chan Pdf, mensagem []string, chaveapi string, wg *sync.WaitGroup){
-	contador := 0
+func CallApi(messenger  chan Pdf, message  []string, APIkey string, wg *sync.WaitGroup){
+	counter  := 0
     ctx := context.Background()
     client, err := genai.NewClient(ctx, &genai.ClientConfig{
-        APIKey:  chaveapi,
+        APIKey:  APIkey,
         Backend: genai.BackendGeminiAPI,
     })
     if err != nil {
     log.Printf("erro: %v", err)
     }
   defer client.ClientConfig().HTTPClient.CloseIdleConnections()
-	for _, v := range mensagem {
+	for _, v := range message {
     // Monta o prompt para Gemini
     prompt := fmt.Sprintf("Traduza o seguinte texto para o português brasileiro, sem comentários extras, apenas o texto traduzido, Responda exclusivamente com a tradução, sem introdução, explicação ou comentários., traduzido:\n\n%s", v) 
     result, err := client.Models.GenerateContent(
@@ -31,12 +31,12 @@ func CallApi(mensageiro chan Pdf, mensagem []string, chaveapi string, wg *sync.W
     if err != nil {
  log.Printf("erro: %v", err)
     }
-		contador += 1
-		mensageiro <- Pdf{
-			Indice: contador,
-			Texto: result.Text(),
+		counter += 1
+		messenger <- Pdf{
+			Indice: counter,
+			Text: result.Text(),
 		}
 	}
-	close(mensageiro)
+	close(messenger)
 	wg.Done()
 }
